6d5b34c8cd5f0e1c0a190471ade468006d61af8e
jeetganatra <jeetganatra138@gmail.com>
2021-05-21 20:57:27 +0530

    Some colour changes

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,31 +1,49 @@
-import classes from "./styles.module.css"
-import RefreshIcon from '@material-ui/icons/Refresh';
+import classes from "./styles.module.css";
+import RefreshIcon from "@material-ui/icons/Refresh";
 import { useState } from "react";
 
 const Main = () => {
-    const [buttonClass, setButtonClass] = useState(`${classes.refreshIcon} ${classes.refreshIconHide}`)
-    const handleOnChange = (e) => {
-        if (e.target.checked) {
-            setButtonClass(`${classes.refreshIcon}`);
-        }
+  const [buttonClass, setButtonClass] = useState(
+    `${classes.refreshIcon} ${classes.refreshIconHide}`
+  );
+  const handleOnChange = (event) => {
+    if (event.target.checked) {
+      setButtonClass(`${classes.refreshIcon}`);
     }
-    return (
-        <div className={classes.button}>
-            <input className={classes["c-checkbox"]} type="checkbox" id="checkbox" onChange={handleOnChange} />
-            <div className={classes["c-formContainer"]}>
-                <form className={classes["c-form"]} action="">
-                    <input className={classes["c-form__input"]} placeholder="E-mail" type="text" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required />
-                    <label className={classes["c-form__buttonLabel"]} for="checkbox">
-                        <button className={classes["c-form__button"]} type="button">Send</button>
-                    </label>
-                    <label className={classes["c-form__toggle"]} for="checkbox" data-title="Notify me"></label>
-                </form>
-                <div className={buttonClass}>
-                    <RefreshIcon />
-                </div>
-            </div>
+  };
+  return (
+    <div className={classes.button}>
+      <input
+        className={classes["c-checkbox"]}
+        type="checkbox"
+        id="checkbox"
+        onChange={handleOnChange}
+      />
+      <div className={classes["c-formContainer"]}>
+        <form className={classes["c-form"]} action="">
+          <input
+            className={classes["c-form__input"]}
+            placeholder="CF Handle"
+            type="text"
+            required
+          />
+          <label className={classes["c-form__buttonLabel"]} for="checkbox">
+            <button className={classes["c-form__button"]} type="button">
+              Submit
+            </button>
+          </label>
+          <label
+            className={classes["c-form__toggle"]}
+            for="checkbox"
+            data-title="Enter CF Handle"
+          ></label>
+        </form>
+        <div className={buttonClass}>
+          <RefreshIcon />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;

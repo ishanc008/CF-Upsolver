cd32c49e76d5151e225dbced3f21cca87ff91485
jeetganatra <jeetganatra138@gmail.com>
2021-05-26 19:07:22 +0530

    Made some styling changes and updated recommended problems logic

--- src/Components/Recommended/Problems.js
+++ src/Components/Recommended/Problems.js
@@ -1,122 +1,156 @@
-import axios from "axios"
+import axios from "axios";
 
 export const getRecommendedProblems = async (userHandle) => {
-    const res = await axios.get(`https://codeforces.com/api/user.status?handle=${userHandle}&from=1`);
-    //console.log(res);
-    const numberOfSubs = res.data.result.length;
+  const res = await axios.get(
+    `https://codeforces.com/api/user.status?handle=${userHandle}&from=1`
+  );
+  //console.log(res);
+  const numberOfSubs = res.data.result.length;
 
-    const allProbs = [];
-    for (var x = 0; x < numberOfSubs; x++) {
-        allProbs.push(res.data.result[x]);
+  const allProbs = [];
+  for (var x = 0; x < numberOfSubs; x++) {
+    allProbs.push(res.data.result[x]);
+  }
+  const ACprobs = [];
+
+  for (var i = 0; i < numberOfSubs; i++) {
+    if (res.data.result[i].verdict === "OK") {
+      const Problem = {
+        contestId: res.data.result[i].author.contestId,
+        name: res.data.result[i].problem.name,
+        probIdx: res.data.result[i].problem.index,
+      };
+      ACprobs.push(Problem);
     }
-    const ACprobs = [];
+  }
 
-    for (var i = 0; i < numberOfSubs; i++) {
-        if (res.data.result[i].verdict === "OK") {
-            const Problem = {
-                contestId: res.data.result[i].author.contestId,
-                name: res.data.result[i].problem.name,
-                probIdx: res.data.result[i].problem.index,
-            };
-            ACprobs.push(Problem);
-        }
+  //console.log(ACprobs.length);
+
+  const unSolvedProbs = [];
+
+  const probSet = await axios.get(
+    "https://codeforces.com/api/problemset.problems"
+  );
+  //console.log(probSet);
+  const totalProbs = probSet.data.result.problems.length;
+  for (let i = 0; i < totalProbs; i++) {
+    let ok = false;
+    for (let j = 0; j < ACprobs.length; j++) {
+      if (
+        ACprobs[j].contestId === probSet.data.result.problems[i].contestId &&
+        ACprobs[j].probIdx === probSet.data.result.problems[i].index
+      ) {
+        ok = true;
+        break;
+      }
     }
-
-    //console.log(ACprobs.length);
-
-    const unSolvedProbs = [];
-
-    const probSet = await axios.get("https://codeforces.com/api/problemset.problems");
-    //console.log(probSet);
-    const totalProbs = probSet.data.result.problems.length;
-    for (let i = 0; i < totalProbs; i++) {
-        let ok = false;
-        for (let j = 0; j < ACprobs.length; j++) {
-            if (ACprobs[j].contestId === probSet.data.result.problems[i].contestId && ACprobs[j].probIdx === probSet.data.result.problems[i].index) {
-                ok = true;
-                break;
-            }
-        }
-        if (ok === false) {
-            unSolvedProbs.push(probSet.data.result.problems[i]);
-        }
+    if (ok === false) {
+      unSolvedProbs.push(probSet.data.result.problems[i]);
     }
-    //console.log(unSolvedProbs);
+  }
+  //console.log(unSolvedProbs);
 
-    const rating = await axios.get(`https://codeforces.com/api/user.rating?handle=${userHandle}`);
-    const ratingArray = rating.data.result;
-    const newRating = ratingArray.map(user => user.newRating);
-    newRating.sort();
-    newRating.reverse();
-    let avgRating, rate = 0;
-    let length = (newRating.length);
-    if (newRating.length > 30) {
-        length = Math.floor(length / 3);
+  const rating = await axios.get(
+    `https://codeforces.com/api/user.rating?handle=${userHandle}`
+  );
+  const ratingArray = rating.data.result;
+  const newRating = ratingArray.map((user) => user.newRating);
+  newRating.sort();
+  newRating.reverse();
+  let avgRating,
+    rate = 0;
+  let length = newRating.length;
+  if (newRating.length > 30) {
+    length = Math.floor(length / 3);
+  }
+  for (let i = 0; i < length; i++) {
+    rate += newRating[i];
+  }
+  avgRating = Math.floor(rate / length);
+  avgRating = Math.round(avgRating / 100) * 100;
+  console.log(avgRating);
+
+  const easyProbs = [];
+  const mediumProbs = [];
+  const hardProbs = [];
+  const veryHardProbs = [];
+
+  for (let i = 8; i < unSolvedProbs.length; i++) {
+    if (avgRating < 1000) {
+      if (unSolvedProbs[i].rating <= 1000) {
+        easyProbs.push(unSolvedProbs[i]);
+      } else if (
+        unSolvedProbs[i].rating > 1000 &&
+        unSolvedProbs[i].rating <= 1300
+      ) {
+        mediumProbs.push(unSolvedProbs[i]);
+      } else if (
+        unSolvedProbs[i].rating > 1300 &&
+        unSolvedProbs[i].rating <= 1800
+      ) {
+        hardProbs.push(unSolvedProbs[i]);
+      } else {
+        veryHardProbs.push(unSolvedProbs[i]);
+      }
     }
-    for (let i = 0; i < length; i++) {
-        rate += newRating[i];
+    if (avgRating > 1000 && avgRating < 2000) {
+      if (unSolvedProbs[i].rating < avgRating - 200) {
+        easyProbs.push(unSolvedProbs[i]);
+      } else if (
+        unSolvedProbs[i].rating >= avgRating - 200 &&
+        unSolvedProbs[i].rating <= avgRating + 200
+      ) {
+        mediumProbs.push(unSolvedProbs[i]);
+      } else if (
+        unSolvedProbs[i].rating > avgRating + 200 &&
+        unSolvedProbs[i].rating <= avgRating + 600
+      ) {
+        hardProbs.push(unSolvedProbs[i]);
+      } else {
+        veryHardProbs.push(unSolvedProbs[i]);
+      }
+    } else if (avgRating > 2000 && avgRating < 3000) {
+      if (unSolvedProbs[i].rating <= 1600) {
+        easyProbs.push(unSolvedProbs[i]);
+      } else if (
+        unSolvedProbs[i].rating > 1600 &&
+        unSolvedProbs[i].rating <= 2200
+      ) {
+        mediumProbs.push(unSolvedProbs[i]);
+      } else if (
+        unSolvedProbs[i].rating > 2200 &&
+        unSolvedProbs[i].rating <= 2800
+      ) {
+        hardProbs.push(unSolvedProbs[i]);
+      } else {
+        veryHardProbs.push(unSolvedProbs[i]);
+      }
+    } else if (avgRating > 3000) {
+      if (unSolvedProbs[i].rating <= 1800) {
+        easyProbs.push(unSolvedProbs[i]);
+      } else if (
+        unSolvedProbs[i].rating > 1800 &&
+        unSolvedProbs[i].rating <= 2400
+      ) {
+        mediumProbs.push(unSolvedProbs[i]);
+      } else if (
+        unSolvedProbs[i].rating > 2400 &&
+        unSolvedProbs[i].rating <= 2900
+      ) {
+        hardProbs.push(unSolvedProbs[i]);
+      } else {
+        veryHardProbs.push(unSolvedProbs[i]);
+      }
     }
-    avgRating = Math.floor(rate / length);
-    avgRating = Math.round(avgRating / 100) * 100;
-    console.log(avgRating);
+  }
 
-    const easyProbs = [];
-    const mediumProbs = [];
-    const hardProbs = [];
-    const veryHardProbs = [];
+  const recommendedProblemData = {
+    easyProbs,
+    mediumProbs,
+    hardProbs,
+    veryHardProbs,
+  };
 
-    for (let i = 8; i < unSolvedProbs.length; i++) {
-        if (avgRating < 2000) {
-            if (unSolvedProbs[i].rating < avgRating - 200) {
-                easyProbs.push(unSolvedProbs[i]);
-            }
-            else if (unSolvedProbs[i].rating >= avgRating - 200 && unSolvedProbs[i].rating <= avgRating + 200) {
-                mediumProbs.push(unSolvedProbs[i]);
-            }
-            else if (unSolvedProbs[i].rating > avgRating + 200 && unSolvedProbs[i].rating <= avgRating + 600) {
-                hardProbs.push(unSolvedProbs[i]);
-            }
-            else {
-                veryHardProbs.push(unSolvedProbs[i]);
-            }
-        }
-        else if (avgRating > 2000 && avgRating < 3000) {
-            if (unSolvedProbs[i].rating <= 1600) {
-                easyProbs.push(unSolvedProbs[i]);
-            }
-            else if (unSolvedProbs[i].rating > 1600 && unSolvedProbs[i].rating <= 2200) {
-                mediumProbs.push(unSolvedProbs[i]);
-            }
-            else if (unSolvedProbs[i].rating > 2200 && unSolvedProbs[i].rating <= 2800) {
-                hardProbs.push(unSolvedProbs[i]);
-            }
-            else {
-                veryHardProbs.push(unSolvedProbs[i]);
-            }
-        }
-        else if (avgRating > 3000) {
-            if (unSolvedProbs[i].rating <= 1800) {
-                easyProbs.push(unSolvedProbs[i]);
-            }
-            else if (unSolvedProbs[i].rating > 1800 && unSolvedProbs[i].rating <= 2400) {
-                mediumProbs.push(unSolvedProbs[i]);
-            }
-            else if (unSolvedProbs[i].rating > 2400 && unSolvedProbs[i].rating <= 2900) {
-                hardProbs.push(unSolvedProbs[i]);
-            }
-            else {
-                veryHardProbs.push(unSolvedProbs[i]);
-            }
-        }
-    }
-
-    const recommendedProblemData = {
-        easyProbs,
-        mediumProbs,
-        hardProbs,
-        veryHardProbs,
-    };
-
-    //console.log(recommendedProblemData);
-    return recommendedProblemData;
-}
+  //console.log(recommendedProblemData);
+  return recommendedProblemData;
+};
--- src/Components/Recommended/Questions/styles.js
+++ src/Components/Recommended/Questions/styles.js
@@ -1,22 +1,22 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(1),
-        color: "red",
-        borderColor: "red",
-        background: "#0f2027",
-        background: "linear-gradient(to right, #0f2027, #203a43, #2c5364)",
-    },
-    chip: {
-        margin: theme.spacing(2),
-    },
-    typography: {
-        color: "white",
-        marginTop: "5%",
-        fontFamily: "cursive",
-    }
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    color: "red",
+    borderColor: "red",
+    background: "#0f2027",
+    background: "linear-gradient(to right, #0f2027, #203a43, #2c5364)",
+  },
+  chip: {
+    margin: theme.spacing(2),
+  },
+  typography: {
+    color: "white",
+    marginTop: "5%",
+    fontFamily: "cursive",
+  },
 }));
